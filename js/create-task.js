81101289d5ca73255042e38dfd01c3e83dac9a91
Evan Sun <esun@ucvts.tec.nj.us>
2019-02-28 10:49:43 -0500

    worked on css and html

---
+++ js/create-task.js
@@ -0,0 +1,6 @@
+function answer(){
+  document.getElementById("ans1").innerHTML = "oof";
+  document.getElementById("ans2").innerHTML = "oof";
+  document.getElementById("ans3").innerHTML = "oof";
+  document.getElementById("ans4").innerHTML = "oof";
+}
