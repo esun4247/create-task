a86b1b4bc2cb2e61d566207c412797bf746ed3ee
Evan Sun <esun@ucvts.tec.nj.us>
2019-03-08 10:45:09 -0500

    finished javascript

--- js/create-task.js
+++ js/create-task.js
@@ -1,38 +1,97 @@
 var fques = 0;
 var ques;
 var check;
+var t;
+var seconds = 0;
+var minutes = 0;
+var hours = 0;
+var score = 0;
+var buttonClicked = 0;
 
 function checkAns(x){
-  if(x == check){
-    document.getElementById("correct").innerHTML = "Correct!";
-  }else{
-    document.getElementById("correct").innerHTML = "False!";
+  if(buttonClicked == 0){
+    if(x == check){
+      document.getElementById("correct").innerHTML = "<br/>Answer:<br/>Correct!";
+      score++;
+      document.getElementById("ansCount").innerHTML = "<br/>Score:<br/>" + score;
+    }else{
+      document.getElementById("correct").innerHTML = "<br/>Answer:<br/>False!";
+      score--;
+      document.getElementById("ansCount").innerHTML = "<br/>Score:<br/>" + score;
+    }
+    buttonClicked = 1;
   }
 }
 
-function question(){
-    if(fques == 0){
-      genquest();
-      genansw();
-      fques = 1;
-    }else{
+function restart(){
+  stopTimer();
+  document.getElementById("time").innerHTML = "<br/>Time:<br/>00:00:00";
+  document.getElementById("correct").innerHTML = "<br/>Answer<br/>-";
+  document.getElementById("ansCount").innerHTML = "<br/>Score<br/>0";
+  document.getElementById("start").innerHTML = "Start";
+  document.getElementById("ans1").innerHTML = "";
+  document.getElementById("ans2").innerHTML = "";
+  document.getElementById("ans3").innerHTML = "";
+  document.getElementById("ans4").innerHTML = "";
+  document.getElementById("quest").innerHTML = "";
+  score = 0;
+  buttonClicked = 0;
+  fques = 0;
+}
 
-    }
+function question(){
+  if(fques == 0){
+    startTimer()
+    genquest();
+    genansw();
+    document.getElementById("start").innerHTML = "Next Question";
+    fques = 1;
+  }else{
+    genquest();
+    genansw();
+    buttonClicked = 0;
+    document.getElementById("correct").innerHTML = "<br/>Answer<br/>-"
+  }
+}
+
+function startTimer(){
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+  timer();
+}
+function stopTimer(){
+  clearTimeout(t);
 }
 
 function timer(){
+  document.getElementById("time").innerHTML = "<br/>Time:<br/>" + (hours ? (hours>9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes>9 ? minutes : "0" + minutes) : "00") + ":" + (seconds>9 ? seconds : "0" + seconds);;
+  seconds++;
+  if(seconds >= 60){
+    seconds = 0;
+    minutes++;
+    console.log("oof");
+  }
+  if(minutes >= 60){
+    minutes = 0;
+    hours++;
+    console.log("oof");
+  }
+  t = setTimeout(function(){ timer() }, 1000);
 }
 
 function genquest(){
   ques = genopp();
   document.getElementById("quest").innerHTML = ques;
 }
-
 function genansw(){
   let answer = eval(ques);
-  let ans1 = eval(genopp());
-  let ans2 = eval(genopp());
-  let ans3 = eval(genopp());
+  let w = eval(genopp());
+  let ans1 = (w != answer) ? w : w+1;
+  let y = eval(genopp());
+  let ans2 = (y != answer) ? y : y+8;
+  let z = eval(genopp());
+  let ans3 = (z != answer) ? z : z-6;
   let x = Math.floor(Math.random()*4);
   switch(x){
     case 0:
